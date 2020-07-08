99f231d0609602e5c8cdfe5f5b7b76dee02fbc23
Giacomob85 <giacomoimmobilt@gmail.com>
2020-07-08 12:30:28 +0200

    primo commit

---
+++ build/precache-manifest.5040f797c5c98b84b41be16bc039c175.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789b53a02e58598aaa5477003ea02461",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2316e8e242f8317a6cf5",
+    "url": "/static/css/2.3a221eca.chunk.css"
+  },
+  {
+    "revision": "a2a89181f8acd1b1d3c5",
+    "url": "/static/css/main.b10d9964.chunk.css"
+  },
+  {
+    "revision": "2316e8e242f8317a6cf5",
+    "url": "/static/js/2.f955162b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.f955162b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2a89181f8acd1b1d3c5",
+    "url": "/static/js/main.3d58baa7.chunk.js"
+  },
+  {
+    "revision": "8d12a083c9f71208a8a3",
+    "url": "/static/js/runtime-main.3d3338e4.js"
+  },
+  {
+    "revision": "756b6e262d8e5e7686089fe4be8b3069",
+    "url": "/static/media/books-1281581_1280.756b6e26.jpg"
+  },
+  {
+    "revision": "5eb7e47f720fd110cc237c1595869209",
+    "url": "/static/media/old-books-436498_1920.5eb7e47f.jpg"
+  }
+]);
---
+++ src/Components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+	return (
+		<div className='main-footer'>
+			<div className='container'>
+				<div className='row'>
+					<div className='col'>	
+						<p><i class="fas fa-beer"></i> Bookaneer</p>				
+					</div>	
+					<div className='col'>
+						<p><i class="far fa-copyright"></i> 2020 All right reserved</p>
+					</div>
+					<div className='col'>
+						<p>Crafted with <i class="fas fa-heart"></i> e <i class="fas fa-mug-hot"></i> for Real Bookaneer</p>
+						</div>		
+						
+			</div>
+		</div>
+		</div>
+	)
+}
+export default Footer;
